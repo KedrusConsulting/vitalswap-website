2cace8e727e6c10643e62a9510a7311c82839587
iamwhitegod <kmbagwu12@gmail.com>
2022-11-29 23:09:44 +0100

    Completed terms page

--- src/pages/Terms/index.jsx
+++ src/pages/Terms/index.jsx
@@ -1,7 +1,278 @@
 import React from "react";
+import Footer from "../../components/Footer";
+
+import Navigation from "../../components/Navigation";
 
 function Terms() {
-  return <div>Terms</div>;
+  return (
+    <>
+      <div className="header--terms">
+        <Navigation />
+
+        <div className="hero--terms">
+          <h1 className="heading--primary">Terms of service</h1>
+          <p>Effective date: 3rd of June, 2022</p>
+        </div>
+      </div>
+
+      <main className="main--terms">
+        <div className="wrapper">
+          <p className="mb-4">
+            These Terms of Use (“Terms”) govern your use of ‘’VitalSwap”
+            operated by VitalSwap Technologies when you use the App on any
+            mobile phone, tablet, watch or other devices (“Device”) that
+            supports the App. These Terms are a legal agreement, so please read
+            them carefully. The words “you” and “your” mean a customer or
+            authorized user, and the words “we”, “us”, “our”,” VitalSwap” mean
+            VitalSwap Technologies.
+          </p>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Terms That Apply.</h5>
+            <p>
+              In addition to these terms, you agree that when you use this App,
+              you will remain subject to the terms and conditions of all your
+              existing agreements with VitalSwap.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Changes to Terms.</h5>
+            <p>
+              In addition to these terms, you agree that when you use this App,
+              you will remain subject to the terms and conditions of all your
+              existing agreements with VitalSwap.VitalSwap may modify these
+              Terms at any time. Your continued use of our Services will
+              indicate your acknowledgement of, and intent to be bound by, any
+              modifications made after your initial acceptance of these Terms.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Privacy.</h5>
+            <p>
+              Any personal information provided to us shall be subject to the
+              terms and conditions of our privacy policy.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Unauthorized Use.</h5>
+            <p>
+              You agree not to use or allow others to use Vital Swap in any way
+              that would violate any agreement you have with us, including, but
+              not limited to, any prohibition on fraudulent or illegal activity
+              contained in the agreement(s) governing your use of the App. If
+              you permit other persons to use your App, you are responsible for
+              any transactions they authorize to the extent permitted by the
+              applicable law and the agreement(s) controlling the Card.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">
+              Account Ownership/Accurate Information.
+            </h5>
+
+            <p className="mb-4">
+              You represent that you are the legal owner of the account and
+              other financial information which is provided to VitalSwap. You
+              represent and agree that all information you provide in connection
+              with the VitalSwap is accurate, current, and complete and that you
+              have the right to provide such information to us to operate the
+              App. You agree not to misrepresent your identity or your account
+              information. You agree that we may decide to block, deactivate or
+              restrict your access to Vital Swap if you violate our policy.
+              Providing false information may cause your bank or payment
+              processor to hold your funds. You agree to resolve such
+              misrepresentation with your bank or payment processor.
+            </p>
+
+            <h6>Support</h6>
+
+            <p>
+              Please call us at 725-234-0077 for support on any issues related
+              to your use of VitalSwap or if your device is lost, stolen or
+              compromised.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Third-Party Fees.</h5>
+            <p>
+              You are responsible for any fees charged by any third party in
+              connection with your use of a VitalSwap, including, without
+              limitation, your mobile telephone service provider and Internet
+              provider.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">
+              Promotion, Referrals and Other Bonus.
+            </h5>
+            <p>
+              VitalSwap shall pay promotional, referrals and other bonuses
+              (referred to as bonuses) to you solely at our discretion and will
+              not be responsible for any cost incurred by you to obtain such
+              bonuses.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Severability Clause.</h5>
+            <p className="mb-3">
+              If any term, provision, covenant or condition of these Terms is
+              held by a court of competent jurisdiction to be invalid, void or
+              unenforceable, the remainder of the provisions of these terms
+              shall remain in full force and effect and shall in no way be
+              affected, impaired or invalidated. You agree to allow us to
+              contact you electronically.
+            </p>
+
+            <p className="mb-3">
+              You agree to receive electronic communications from us, including
+              emails, phone calls and SMS, to the email address you have
+              provided in connection with your VitalSwap account. These
+              electronic communications will relate to your use of VitalSwap.
+              You agree to update your email address when it changes by
+              contacting us using the contact information in your Account
+              Agreement. You may also contact us if you wish to withdraw your
+              consent to receive these electronic communications, but doing so
+              may result in your inability to continue to use VitalSwap.
+            </p>
+
+            <p className="mb-3">
+              We may block, suspend, or cancel your use of services on
+              VitalSwap.
+            </p>
+
+            <p className="mb-3">
+              We may block you from creating swap deals, depositing to your
+              VitalSwap Wallet, or suspend your ability to use VitalSwap to make
+              purchases, Swaps, or cancel entirely your ability to continue to
+              use VitalSwap. We may take these actions at any time and for any
+              reason relating to your inappropriate use of the app, which
+              includes engaging in illegal transactions on the app, if we
+              suspect fraud with your use of VitalSwap, if you have an overdue
+              or negative balance on your account, or if applicable laws change.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Security Of The Wallet.</h5>
+            <p>
+              In addition to your efforts to keep your credentials secure, we
+              take reasonable steps to help ensure that information we send to
+              third-party from your use of VitalSwap is sent securely. However,
+              the third-party service provider is responsible for the security
+              of the information it provides.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Tax and Local Laws.</h5>
+            <p>
+              As your local laws require, you are responsible for fulfilling tax
+              obligations in the country where you operate your VitalSwap
+              wallet.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Account Security.</h5>
+            <p>
+              Security on our platform is a shared responsibility. While we
+              ensure that your data is secured at rest and transit, it is your
+              responsibility to protect your credentials and devices from
+              Unauthorized usage. We assume you authorize all transactions
+              initiated by your device.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Zelle.</h5>
+            <p className="mb-3">
+              Partner's Disclosure: You are sending funds to ABetaGlobal LLC.
+              ABetaGlobal LLC is a related party service provider to VitalSwap
+              LLC. ABetaGlobal is *not* a bank or payment processor. Funds sent
+              to ABetaGlobal are custodians at banks that are FDIC-insured.
+              After funds sent are received and settled by ABetaGlobal,
+              VitalSwap LLC will credit the corresponding user account. We will
+              decline funds sent from bank accounts that are not connected to
+              your VitalSwap user account.
+            </p>
+
+            <p>
+              <strong>Timing:</strong> While most transfers will be settled the
+              same day, funds sent to ABetaGlobal may take up to 2 days for
+              settlement. We cannot estimate or guarantee the exact timing of
+              when funds will be available due to varying settlements for
+              transfers from each bank. Learn more about Zelle's User Service
+              Agreement.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">
+              Negative Balances, Charges and Overdrafts
+            </h5>
+            <p>
+              You agree to refund VitalSwap all negative balances in your
+              wallets maintained by VitalSwap via the mobile app, and you
+              authorize VitalSwap to charge your bank account for all negative
+              balances in your wallet. In the event that you incur overdraft
+              charges in your bank account as a result of charging the account
+              you provided to us, you will be responsible for such.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">Refund</h5>
+            <p>
+              For values not yet consumed, provided or transferred outside of
+              our platform, We will issue a refund at your request on our app or
+              through our help channels.
+            </p>
+          </div>
+
+          <div className="text-block">
+            <h5 className="text-block__heading">
+              DISCLAIMER OF WARRANTIES AND LIMITATION OF LIABILITY
+            </h5>
+            <p>
+              YOU UNDERSTAND AND AGREE THAT YOUR USE OF VITALSWAP IS AT YOUR
+              RISK. WE ARE ONLY RESPONSIBLE FOR PROVIDING VIRTUAL CURRENCY
+              EXCHANGES AS EXPRESSLY STATED IN THE AGREEMENT. THERE IS NO
+              GUARANTEE THAT ACCESS TO VITALSWAP WILL BE AVAILABLE AT ALL TIMES,
+              AND WE SHALL NOT BE LIABLE IF YOU ARE UNABLE TO ACCESS the app.
+              VITALSWAP IS PROVIDED "AS IS" AND, EXCEPT AS PROHIBITED BY LAW,
+              WE, OUR EMPLOYEES, MANAGES, DIRECTORS, SHAREHOLDERS, AND OUR
+              THIRD-PARTY SERVICE PROVIDERS DISCLAIM ANY EXPRESS OR IMPLIED
+              WARRANTIES CONCERNING VITALSWAP, EQUIPMENT OR SOFTWARE, INCLUDING,
+              BUT NOT LIMITED TO ANY WARRANTIES OR USEFULNESS, FITNESS FOR A
+              PARTICULAR PURPOSE OR NON-INFRINGEMENT OF ANY PARTIES' PROPRIETARY
+              RIGHTS. IN NO CASE SHALL VITALSWAP OR ANY OF OUR THIRD-PARTY
+              SERVICE PROVIDERS BE LIABLE FOR ANY LOSS OF DATA, MONEY, PROFIT,
+              GOODWILL, OR SPECIAL, PUNITIVE, INDIRECT, EXEMPLARY OR
+              CONSEQUENTIAL DAMAGES OF ANY KIND OR NATURE SUFFERED BY YOU
+              ARISING OUT OF OR RELATED TO THIS AGREEMENT, VITALSWAP, THE
+              SOFTWARE, THE EQUIPMENT OR VITALSWAP WALLET WHETHER OR NOT SUCH
+              CLAIM FOR DAMAGES IS BASED ON TORT OR CONTRACTOR OR WHETHER WE HAD
+              BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES OR SHOULD HAVE
+              KNOWN OF THE LIKELIHOOD OF SUCH DAMAGES, EXCEPT AS MAY BE REQUIRED
+              BY LAW. IN STATES THAT DO NOT ALLOW THE EXCLUSION OR LIMITATION OF
+              LIABILITY FOR INDIRECT SPECIAL OR CONSEQUENTIAL DAMAGES, VITALSWAP
+              AND OUR THIRD-PARTY SERVICE PROVIDER'S LIABILITY IS LIMITED TO THE
+              EXTENT PERMITTED BY LAW.
+            </p>
+          </div>
+        </div>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Terms;
