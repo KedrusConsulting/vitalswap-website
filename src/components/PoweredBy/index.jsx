a00ef12f8805b57ab29da1c34623812c079cae6f
iamwhitegod <kmbagwu12@gmail.com>
2024-09-23 18:24:44 +0100

    chore: fix partners logo

--- src/components/PoweredBy/index.jsx
+++ src/components/PoweredBy/index.jsx
@@ -1,21 +1,14 @@
-import batesGroupLogo from "../../assets/batesgroup-logo.png";
+import plaidLogo from "../../assets/plaid-logo-horizontal-RGB 1@2x.png";
 import dojahLogo from "../../assets/dojah.png";
-import elevateVenturesLogo from "../../assets/elevate-ventures.png";
-import beNimbleCoLogo from "../../assets/be-nimble-co.png";
+
 import googleLogo from "../../assets/google.png";
 import awsLogo from "../../assets/aws.png";
-import slackLogo from "../../assets/slack.png";
-import safeHeavenLogo from "../../assets/safe-haven-mfb.png";
 
 const images = [
-  { name: "Bates group logo", src: batesGroupLogo },
   { name: "Dojah logo", src: dojahLogo },
-  { name: "Elevate ventures logo", src: elevateVenturesLogo },
-  { name: "Be nimble co logo", src: beNimbleCoLogo },
   { name: "Google logo", src: googleLogo },
+  { name: "Plaid logo", src: plaidLogo },
   { name: "AWS logo", src: awsLogo },
-  { name: "Slack logo", src: slackLogo },
-  { name: "Safe Heaven logo", src: safeHeavenLogo },
 ];
 
 const PoweredBy = () => {
